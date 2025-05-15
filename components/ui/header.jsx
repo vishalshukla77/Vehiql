03b084df2fbce7f3f03c364e3a806a20a735456f
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2025-05-15 20:23:42 +0530

    waitlist page

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import {Button} from "../ui/button";
 import {
   SignedIn,
   SignedOut,
@@ -6,17 +9,82 @@ import {
   SignUpButton,
   UserButton
 } from "@clerk/nextjs";
+import { ArrowLeft, CarFront, Heart, Layout } from 'lucide-react';
 
-export default function Header() {
+
+
+export default function Header({isAdminPage=false}) {
+
+  const isAdmin=false;
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+   <header className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+    <nav className='mx-auto px-4 py-4 flex items-center justify-between '>
+      <Link href={isAdminPage?"/admin":"/"} className='flex'>
+      <Image src={"/logo.png"} alt="vehiql logo" width={200} height={60} className="h-12 w-auto object-cover"/>
+{isAdminPage && (
+  <span className='text-xs font-extralight'>
+    admin
+  </span>
+)}
+
+      </Link>
+      <div className='flex items-center space-x-4'>
+{isAdminPage ? (
+    <Link href="/">
+          <Button variant="outline" className="flex items-center gap-2">
+            <ArrowLeft size={18}/>
+           <span className='hidden md:inline'>Back To App</span>
+          </Button>
+          </Link>
+)
+:(
+
+        <SignedIn>
+
+          
+
+          <Link href="/saved-cars">
+          <Button>
+            <Heart size={18}/>
+           <span className='hidden md:inline'> Saved Cars</span>
+          </Button>
+          </Link>
+          {!isAdmin ? (
+            <Link href="/reservations">
+          <Button variant="outline">
+            <CarFront size={18}/>
+           <span className='hidden md:inline'>My Reservations</span>
+          </Button>
+          </Link>):(
+            <Link href="/admin">
+          <Button variant="outline">
+            <Layout size={18}/>
+           <span className='hidden md:inline'>Admin Portal</span>
+          </Button>
+          </Link>)
+          }
+
+
+        </SignedIn>)}
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl='/'>
+          
+          <Button varaint="outline">
+            Login
+          </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton appearance={{elements:{
+            avatarBox:"W-10 H-10",
+          }}}/>
+
+        
+        </SignedIn>
+
+      </div>
+    </nav>
+   </header>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers(){
+        return [
+            {source:"/embed",
+                headers:[
+                    {
+                    key:"Contect-Security-Policy",
+                    value:"frame-src"
+                    }
+                ]
+
+            }
+        ]
+    }
+};
 
 export default nextConfig;
